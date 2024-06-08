3647e2c0d38ae25ff9957fb96a606acc933b948c
Stephano <nafetsgary@outlook.es>
2024-06-08 16:46:55 -0500

    iniciando examen

---
+++ src/main/java/pe/edu/cibertec/dswii_cl1soap_agueroarcestephano/DswiiCl1SoapAgueroArceStephanoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dswii_cl1soap_agueroarcestephano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapAgueroArceStephanoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswiiCl1SoapAgueroArceStephanoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/dswii_cl1soap_agueroarcestephano/DswiiCl1SoapAgueroArceStephanoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.dswii_cl1soap_agueroarcestephano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapAgueroArceStephanoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
