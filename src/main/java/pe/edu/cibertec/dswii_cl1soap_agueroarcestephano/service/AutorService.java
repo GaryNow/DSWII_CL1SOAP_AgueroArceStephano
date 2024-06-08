7dfbfda9c419f16db9be552fe98a6dad8d705cef
Stephano <nafetsgary@outlook.es>
2024-06-08 17:00:01 -0500

    Pregunta 3 completada

--- src/main/java/pe/edu/cibertec/dswii_cl1soap_agueroarcestephano/service/AutorService.java
+++ src/main/java/pe/edu/cibertec/dswii_cl1soap_agueroarcestephano/service/AutorService.java
@@ -1,4 +1,40 @@
 package pe.edu.cibertec.dswii_cl1soap_agueroarcestephano.service;
 
-public class AutorService {
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.dswii_cl1soap_agueroarcestephano.convert.AutorConvert;
+import pe.edu.cibertec.dswii_cl1soap_agueroarcestephano.model.Autor;
+import pe.edu.cibertec.dswii_cl1soap_agueroarcestephano.repository.AutorRepository;
+import pe.edu.cibertec.ws.objects.Autorws;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class AutorService implements IAutorService {
+
+    private AutorRepository autorRepository;
+    private AutorConvert autorConvert;
+
+    @Override
+    public List<Autorws> listarAutores() {
+        return autorConvert.convertAutorToAutorws(autorRepository.findAll());
+    }
+
+    @Override
+    public Autorws obtenerAutorxId(int id) {
+        Optional<Autor> autor = autorRepository.findById(id);
+        System.out.println(autor);
+        return autor.map(value -> autorConvert.convertAutorToAutorws(value)).orElse(null);
+    }
+
+    @Override
+    public Autorws registrarActualizarAutor(Autorws autorws) {
+        Autor nuevoAutor = autorRepository.save(autorConvert.convertAutorwsToAutor(autorws));
+        if(nuevoAutor == null){
+            return null;
+        }
+        return autorConvert.convertAutorToAutorws(nuevoAutor);
+    }
 }
--- src/main/java/pe/edu/cibertec/dswii_cl1soap_agueroarcestephano/service/IAutorService.java
+++ src/main/java/pe/edu/cibertec/dswii_cl1soap_agueroarcestephano/service/IAutorService.java
@@ -1,4 +1,14 @@
 package pe.edu.cibertec.dswii_cl1soap_agueroarcestephano.service;
 
+import pe.edu.cibertec.ws.objects.Autorws;
+
+import java.util.List;
+
 public interface IAutorService {
+
+    List<Autorws> listarAutores();
+
+    Autorws obtenerAutorxId(int id);
+
+    Autorws registrarActualizarAutor(Autorws autorws);
 }
